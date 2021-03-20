d5021e75f1c8b78f27df9075f307285fe43b8234
Zainab <szainabr@gmail.com>
2021-03-19 22:31:37 -0700

    All required elements of Lab 3

--- FlashCards/ViewControllers/FirstScreenViewController.swift
+++ FlashCards/ViewControllers/FirstScreenViewController.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+struct Flashcard {
+    var question: String
+    var answer: String
+    
+}
+
 class FirstScreenViewController: UIViewController {
 
     @IBOutlet weak var FrontLabel: UILabel!
@@ -15,6 +21,11 @@ class FirstScreenViewController: UIViewController {
     @IBOutlet weak var OptionOne: UIButton!
     @IBOutlet weak var OptionTwo: UIButton!
     @IBOutlet weak var OptionThree: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var prevButton: UIButton!
+    
+    var flashcards = [Flashcard]()
+    var currentIndex = 0
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let navigationController = segue.destination as! UINavigationController
@@ -25,6 +36,8 @@ class FirstScreenViewController: UIViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        readSavedFlashcards()
         // Do any additional setup after loading the view.
         card.layer.cornerRadius = 20.0
         card.layer.shadowRadius = 15.0
@@ -47,6 +60,13 @@ class FirstScreenViewController: UIViewController {
         OptionThree.layer.borderWidth = 3.0
         OptionThree.layer.borderColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
         OptionThree.layer.cornerRadius = 20.0
+        
+        if flashcards.count == 0 {
+            updateFlashcard(question: "What's the capital of Pakistan?", answer1: "Lahore", answer2: "Islamabad", answer3: "Karachi")
+        } else {
+            updateLabels()
+            updateNextPrevButtons()
+        }
     }
 
 
@@ -67,12 +87,79 @@ class FirstScreenViewController: UIViewController {
     @IBAction func didTapOnQuestion(_ sender: Any) {
         FrontLabel.isHidden = true
     }
+    
+    @IBAction func didTapOnNext(_ sender: Any) {
+        currentIndex = currentIndex + 1
+        updateLabels()
+        updateNextPrevButtons()
+    }
+    @IBAction func didTapOnPrev(_ sender: Any) {
+        currentIndex = currentIndex - 1
+        updateLabels()
+        updateNextPrevButtons()
+    }
+    
     func updateFlashcard(question: String, answer1: String, answer2: String, answer3: String) {
-        FrontLabel.text = question
-        BackLabel.text = answer2
+        let flashcard = Flashcard(question: question, answer: answer2)
+        
         OptionOne.setTitle(answer1, for: .normal)
         OptionTwo.setTitle(answer2, for: .normal)
         OptionThree.setTitle(answer3, for: .normal)
+        
+        flashcards.append(flashcard)
+        print("Added new flashcard 😎")
+        print("We now have \(flashcards.count) flashcards")
+        
+        currentIndex = flashcards.count - 1
+        print("Our current index is \(currentIndex)")
+        
+        updateNextPrevButtons()
+        saveAllFlashcardsToDisk()
+        updateLabels()
     
     }
+    func updateNextPrevButtons() {
+        if currentIndex == flashcards.count - 1 {
+            nextButton.isEnabled = false
+        }
+        else {
+            nextButton.isEnabled = true
+        }
+        
+        if currentIndex == 0 {
+            prevButton.isEnabled = false
+        }
+        else {
+            prevButton.isEnabled = true
+        }
+    }
+    
+    func updateLabels() {
+        let currentFlashcard = flashcards[currentIndex]
+        FrontLabel.text = currentFlashcard.question
+        BackLabel.text = currentFlashcard.answer
+    }
+
+    
+        
+    func saveAllFlashcardsToDisk() {
+        
+        let dictionaryArray = flashcards.map { (card) -> [String: String] in return ["question": card.question, "answer": card.answer]
+            
+        UserDefaults.standard.set(flashcards, forKey: "flashcards")
+        print("Flashcards saved to UserDefaults")
+        
+        
+            
+        }
+    }
+    func readSavedFlashcards() {
+        if let dictionaryArray = UserDefaults.standard.array(forKey: "flashcards") as? [[String: String]]{
+            let savedCards = dictionaryArray.map { dictionary -> Flashcard in
+                return Flashcard(question: dictionary["question"]!, answer: dictionary["answer"]!)
+            }
+            flashcards.append(contentsOf: savedCards)
+        }
+}
+
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,26 @@
 ## FLASHCARDS_APP
 
 📝 `NOTE:` Paste the README templates for each subsequent lab here at the top, (i.e. lab 2, 3, 4). This will show a history of your development process including which users stories you completed and how your app looked and functioned at each step.
+## Lab 3
+
+### App Description
+Required elements of Lab 3 completed
+
+### App Walk-though
+
+<img src="https://i.imgur.com/skGYIZr.gif" width=200><br>
+
+
+## Required
+- [x] User can browse through multiple flashcards
+- [x] User can re-open the app and see previously created flashcards
+- [x] Push code to GitHub
+
+## Optional
+- [ ] User can delete a flashcard
+- [ ] User can edit existing flashcard
+- [ ] User can store multiple choice questions
+
 ## Lab 2
 
 ### App Description
@@ -8,7 +28,7 @@ All Required elements of Lab 2 done
 
 ### App Walk-though
 
-<img src="https://i.imgur.com/66u2AF3.gif" width=200><br>
+<img src = "https://i.imgur.com/66u2AF3.gif" width = 200><br>
 
 
 ## Required
